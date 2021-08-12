865720a58d13699b9913ad0cdbddcd1c5e4884b3
03hd4658 <82431413+03hd4658@users.noreply.github.com>
2021-08-12 10:21:18 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region  = "us-east-1"
+  profile = "default"
+}
+
+resource "aws_instance" "ec2" {
+  ami           = "ami-0c2b8ca1dad447f8a"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "My-EC2"
+  }
+}
